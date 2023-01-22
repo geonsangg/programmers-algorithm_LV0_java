76e70647aadcc048189675e32f51d56184d9d156
GeonSang Lee <116617354+geonsangg@users.noreply.github.com>
2023-01-22 14:55:01 +0900

    uploaded at 2023. 1. 22. 오후 2:55:11

---
+++ programmers/짝수는 싫어요/solution.java
@@ -0,0 +1,21 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120813
+
+class Solution {
+    public int[] solution(int n) {
+       int[] answer;
+        int index = 0;
+        if(n % 2 == 0){
+        answer = new int [n/2]; 
+            } else{
+          answer = new int [n/2+1];
+        }
+        for(int i=1; i<=n; i++){
+            if(i%2 != 0){
+                answer[index] = i;
+                index++;
+            }
+        }
+        
+        return answer;
+    }
+}
