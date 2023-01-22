3c9aa41c7dc82134e63144e8c5964b80e2bcc97d
GeonSang Lee <116617354+geonsangg@users.noreply.github.com>
2023-01-22 14:53:37 +0900

    uploaded at 2023. 1. 22. 오후 2:53:47

---
+++ programmers/특정 문자 제거하기/solution.java
@@ -0,0 +1,13 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120826
+
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+       for(int i=0; i< my_string.length(); i++) {
+			if(my_string.charAt(i) != letter.charAt(0)) {
+			answer = answer + my_string.charAt(i);
+			}
+		}
+        return answer;
+    }
+}
