63f66caa2853c0184ae919b5a2e651e9ea936be3
GeonSang Lee <116617354+geonsangg@users.noreply.github.com>
2023-02-23 23:06:28 +0900

    uploaded at 2023. 2. 23. 오후 11:07:07

---
+++ programmers/숨어있는 숫자의 덧셈 (2)/solution.java
@@ -0,0 +1,14 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120864
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+		String[] str = my_string.split("[a-zA-Z]");
+		for(int i=0; i<str.length; i++) {
+			if(str[i].matches("[0-9]+")) {
+			answer += Integer.parseInt(str[i]);
+			}
+		}
+        return answer;
+    }
+}
